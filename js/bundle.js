2271f58176c2374b41884b5f9e87927782a033e7
Rod <rcardenas@utexas.edu>
2016-06-23 17:34:21 -0700

    Implement dynamic instructions generator at top of page.

--- js/bundle.js
+++ js/bundle.js
@@ -111,6 +111,18 @@
 	View.prototype.bindEvents = function () {
 	  var view = this;
 	
+	  view.addStartGame();
+	  view.addMusic();
+	  view.addPlayersSelection();
+	
+	  $(".snake").focus();
+	  $(window).on("keydown", function(e){
+	    view.handleKeyPress(e.keyCode);
+	  });
+	};
+	
+	View.prototype.addStartGame = function () {
+	  var view = this;
 	  $(".start-game").on("click", function(e){
 	    e.preventDefault();
 	    if(view.loop === null){
@@ -118,7 +130,9 @@
 	      view.loop = setInterval(view.step.bind(view), 250);
 	    }
 	  });
+	};
 	
+	View.prototype.addMusic = function () {
 	  var audio = document.getElementById("pokesong");
 	  var playpause = false;
 	
@@ -137,19 +151,85 @@
 	      $(".pokesong").text("❚❚");
 	    }
 	  });
+	};
+	
+	View.prototype.addPlayersSelection = function () {
+	  var view = this;
 	
 	  $("#players-label").on("click", function(e){
 	    if($("#players").prop('checked')){
-	      console.log("h v h");
+	      if($("#color").length){
+	        view.removeSnakeColorOption();
+	      }
+	      view.removePlayerInstructions("red");
+	      view.removePlayerInstructions("blue");
+	      view.addPlayerInstructions("red");
+	      $('<div />', { id:"instructions-spacer", class:"spacer"}).appendTo($(".instructions-container"));
+	      view.addPlayerInstructions("blue");
 	    } else {
-	      console.log("h v p");
+	      view.addSnakeColorOption();
 	    }
 	  });
+	};
 	
-	  $(".snake").focus();
-	  $(window).on("keydown", function(e){
-	    view.handleKeyPress(e.keyCode);
+	
+	View.prototype.removeSnakeColorOption = function() {
+	  $("#color").remove();
+	  $("#color-label").remove();
+	  $("#selector-spacer").remove();
+	};
+	
+	View.prototype.addSnakeColorOption = function() {
+	  var container = $(".selectors-container");
+	  var view = this;
+	
+	  //add elements
+	  $('<div />', { id:"selector-spacer", class:"spacer"}).appendTo(container);
+	  $('<input />', { type: 'checkbox', id: "color", class:"tgl tgl-flip"}).appendTo(container);
+	  $('<label />', { 'for': 'color', id: "color-label", tagOff:"웃", tagOn:"웃", class:"tgl-btn"}).appendTo(container);
+	
+	  //remove old instructions
+	  if($("#blue-instructions").length){
+	    view.removePlayerInstructions("blue");
+	  }
+	
+	  if ($("#red-instructions").length) {
+	    view.removePlayerInstructions("red");
+	  }
+	  $("#instructions-spacer").remove();
+	
+	  view.addPlayerInstructions("red");
+	  //add event listener
+	  $("#color-label").on("click", function(e){
+	    if($("#color").prop('checked')){
+	      view.addPlayerInstructions("red");
+	      view.removePlayerInstructions("blue");
+	    } else {
+	      view.addPlayerInstructions("blue");
+	      view.removePlayerInstructions("red");
+	    }
 	  });
+	};
+	
+	View.prototype.addPlayerInstructions = function (color) {
+	  var instructions = color + "-instructions";
+	
+	  $('<div />', { id:instructions, class:"instructions"}).appendTo($(".instructions-container"));
+	
+	  var instructionsEl = $("#" + color + "-instructions");
+	
+	  if (color === "red") {
+	    instructionsEl.append($('<img />', { src:"./img/LUDR.png"}));
+	    instructionsEl.append($('<br />'));
+	    $('<em />', {class:"fire", text:"Red Snake"}).appendTo(instructionsEl);
+	  } else {
+	    instructionsEl.append($('<img />', { src:"./img/WASD.png"}));
+	    instructionsEl.append($('<br />'));
+	    $('<em />', {class:"water", text:"Blue Snake"}).appendTo(instructionsEl);
+	  }
+	};
+	View.prototype.removePlayerInstructions = function (color) {
+	  $("#" + color + "-instructions").remove();
 	};
 	
 	View.prototype.handleKeyPress = function (keyCode) {
